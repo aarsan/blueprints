8de787612de6b34e7653819fe4e63923b3778982
Ahmet Arsan <aarsan@progger.com>
2020-07-06 20:54:28 -0500

    updates

--- create_blueprint.ps1
+++ create_blueprint.ps1
@@ -23,3 +23,14 @@ New-AzBlueprintAssignment -Name "AhmetRG-CentralUS" -Blueprint $publishedBp -Loc
 # Remove-AzResource -Id "/subscriptions/a0fc4f7c-9c55-4151-b900-07761c35237c/providers/Microsoft.Blueprint/blueprints/CreateIdentity"
 
 # Remove-AzResource -Id "/subscriptions/a0fc4f7c-9c55-4151-b900-07761c35237c/providers/Microsoft.Blueprint/blueprints/ahmet-bp"
+
+
+
+
+
+### SAMPLE ###
+# $rg = @{ResourceGroup=@{name='storage_rg';location='eastus'}}
+# $params = @{applytaganditsdefaultvalue_tagName="Department_Cost_Center"; applytaganditsdefaultvalue_tagValue="Contoso/RnD/Dev/986754"}
+# $blueprintObject =  Get-AzBlueprint -SubscriptionId "00000000-1111-0000-1111-000000000000" -Name "myBlueprintName"
+# $secureString = @{mySecureStringParam=@{keyVaultId='/subscriptions/00000000-1111-0000-1111-000000000000/rsourcegroups/myResourceGroup/providers/Microsoft.Keyvault/Vaults/myKeyVault';secretName='mySecret';secretVersion='1.0'}}
+# New-AzBlueprintAssignment -Name "myAssignment" -Blueprint $blueprintObject -SubscriptionId "00000000-1111-0000-1111-000000000000" -Location "West US" -ResourceGroupParameter $rg -Parameter $params -SecureStringParameter $secureString
