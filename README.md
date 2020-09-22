ee4d0b693a2b82a373e9f619d39058cc3ccd2aca
Ahmet Arsan <aarsan@progger.com>
2020-09-22 10:23:23 -0500

    updated readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Blueprint adds a storage account with private link enabled
 
 
 
-# Demo Preparation
+# Demo Preparation (demo_prep.ps1)
 - Creates resource group
 - creates a VNET in that resource group
 
--- demosetup/demo_prep.ps1
+++ demosetup/demo_prep.ps1
@@ -1,6 +1,10 @@
+# Login
+# connect-azAccount
+Select-AzSubscription -SubscriptionId a0fc4f7c-9c55-4151-b900-07761c35237c
+
 # Create Resource Group
 $rgName = "blueprintdemo"
-New-AzResourceGroup -Location "centralus" -Name $rgName
+New-AzResourceGroup -Location "centralus" -Name $rgName -Verbose
 
 # Deploy VNET
 New-AzResourceGroupDeployment -Name "deployvnet" -ResourceGroupName $rgName -TemplateFile .\vnet.json -Verbose
